c2f713d04b403b3810fe16195876c1b66f4f13fc
Suegro24 <marciu199@gmail.com>
2021-02-28 19:43:43 +0100

    Remove router directory

--- server/router/router.ts
+++
@@ -1,9 +0,0 @@
-import * as userController from '../controllers/userController';
-
-const express = require('express');
-const router = express.Router();
-
-const api = '/api/';
-
-// app.use(`${api}/plant`, plantController);
-app.use(`${api}/user`, userController);
