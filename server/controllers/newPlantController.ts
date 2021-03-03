165c479cac894c1e0269fd39b75e6c93e54824fe
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-03-03 22:22:10 +0100

    final corrections of plant controllers

--- server/controllers/newPlantController.ts
+++ server/controllers/newPlantController.ts
@@ -1,43 +1,38 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import * as mongoose from 'mongoose';
-import * as plant from '../models/plant';
+import { Plant } from '../models/plant';
 
 const router = express.Router();
 
 // new plant
-router.post('/', (req: Request, res: Response) => {
-  req.body.accepted = false;
-  const newPlant = new plant.Plant(req.body);
-  newPlant.save((err: Error) => {
+router.post('/', async (req: Request, res: Response) => {
+  const newPlant = await Plant.create(req.body);
+  await newPlant.save((err: Error) => {
     if (err) {
-      return res.status(404).json({ error: 'Plant not saved!' });
+      return res.status(404).json({ error: 'Plant not saved!' }).end();
     }
-    return res.status(200);
+    return res.status(200).json(newPlant).end();
   });
 });
 
-router.put('/:id', (req: Request, res: Response) => {
-  plant.Plant.findByIdAndUpdate(req.params.id, { accepted: true }, { new: true }, (err: Error) => {
+router.put('/:id', async (req: Request, res: Response) => {
+  await Plant.findByIdAndUpdate(req.params.id, { accepted: true }, { new: true }, (err: Error) => {
     if (err) {
-      return res.sendStatus(404).json({ error: 'Plant not saved!' });
+      return res.status(404).json({ error: 'Plant not saved!' }).end();
     }
-    return res.sendStatus(200);
+    return res.status(200).end();
   });
 });
 
-router.delete('/:id', (req: Request, res: Response) => {
-  const plantId = req.params.id;
-  plant.Plant.findByIdAndDelete(
-    plantId,
-    {},
-    (err: Error, plantObject: mongoose.Document) => {
-      if (err) {
-        return res.status(404).json({ error: 'Cannot find this plant!' });
-      }
-      return res.status(200).json({ response: `${plantObject} was deleted.` });
-    }
-  );
+router.delete('/:id', async (req: Request, res: Response) => {
+  const plantId = { _id: req.params.id };
+  const plantToDelete = await Plant.findById(req.params.id);
+
+  if (plantToDelete) {
+    await Plant.findByIdAndRemove(plantId);
+    return res.status(200).json({ response: `${req.params.id} was deleted.` }).end();
+  }
+  return res.status(404).json({ error: 'Cannot find this plant!' }).end();
 });
 
 export default router;
--- server/controllers/plantController.ts
+++ server/controllers/plantController.ts
@@ -1,93 +1,97 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import * as mongoose from 'mongoose';
-import * as plant from '../models/plant';
+import { Plant, Comment } from '../models/plant';
 
 const router = express.Router();
 
 // plant
-router.get('/', (req: Request, res: Response) => {
-  plant.Plant.find({}, (err: Error, plants: mongoose.Document[]) => {
+router.get('/', async (req: Request, res: Response) => {
+  await Plant.find({}, (err: Error, plants: mongoose.Document[]) => {
     if (err) {
-      return res.status(404).json({ error: 'Cannot find plants!' });
+      return res.status(404).json({ error: 'Cannot find plants!' }).end();
     }
-    return res.status(200).json(plants);
+    return res.status(200).json(plants).end();
   });
 });
 
-router.get('/:id', (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   const plantId = req.params.id;
-  plant.Plant.findById(plantId, (err: Error, plantObject: mongoose.Document) => {
+  await Plant.findById(plantId, (err: Error, plantObject: mongoose.Document) => {
     if (err) {
-      return res.status(404).json({ error: `Cannot find plant with id: ${plantId}` });
+      return res.status(404).json({ error: `Cannot find plant with id: ${plantId}` }).end();
     }
-    return res.status(200).json(plantObject);
+    return res.status(200).json(plantObject).end();
   });
 });
 
-router.delete('/:id', (req: Request, res: Response) => {
+router.delete('/:id', async (req: Request, res: Response) => {
   const plantId = req.params.id;
-  plant.Plant.findByIdAndDelete(
+  await Plant.findByIdAndDelete(
     plantId,
     {},
     (err: Error, plantObject: mongoose.Document) => {
       if (err) {
-        return res.status(404).json({ error: 'Cannot find this plant!' });
+        return res.status(404).json({ error: 'Cannot find this plant!' }).end();
       }
-      return res.status(200).json({ response: `${plantObject} was deleted.` });
+      return res.status(200).json({ response: `${plantObject} was deleted.` }).end();
     }
   );
 });
 
 // comment
-router.get('/:id/comments', (req: Request, res: Response) => {
+router.get('/:id/comments', async (req: Request, res: Response) => {
   const plantId = req.params.id;
-  plant.Plant.findById(plantId, (err: Error, plantObject: mongoose.Document) => {
+  await Plant.findById(plantId, (err: Error, plantObject: mongoose.Document) => {
     if (err) {
-      return res.status(404).json({ error: 'Comments not found!' });
+      return res.status(404).json({ error: 'Comments not found!' }).end();
     }
     const plantComments = plantObject.get('comments');
-    return res.status(200).json(plantComments);
+    return res.status(200).json(plantComments).end();
   });
 });
 
-router.post('/:id/comments', (req: Request, res: Response) => {
-  const newComment = new plant.Comment(req.body);
+router.post('/:id/comments', async (req: Request, res: Response) => {
+  const newComment = new Comment(req.body);
   const plantId = req.params.id;
-  plant.Plant.findById(plantId, (err: Error, plantObject: mongoose.Document) => {
+  await Plant.findById(plantId, (err: Error, plantObject: mongoose.Document) => {
     if (err) {
-      return res.sendStatus(404).json({ error: 'Comment not found!' });
+      return res.status(404).json({ error: 'Comment not found!' }).end();
     }
-    const plantComments = plantObject.get('comments');
-    plantComments.push(newComment);
-    return res.status(200);
+    plantObject.get('comments').push(newComment);
+    plantObject.save();
+    return res.status(200).end();
   });
 });
 
-router.delete('/:id/comments/:cid', (req: Request, res: Response) => {
-  const commentId = req.params.cid;
-  plant.Plant.findByIdAndRemove(commentId, {}, (err: Error, commentObj: mongoose.Document) => {
-    if (err) {
-      return res.status(404).json({ error: 'Cannot find this comment!' });
-    }
-    plant.Plant.updateOne({ $pull: { comments: commentId } });
-    return res.status(200).json({ response: `${commentObj} was deleted.` });
-  });
-});
-
-router.put('/comments/:id', (req: Request, res: Response) => {
-  const commentId = req.params.id;
-  plant.Comment.findByIdAndUpdate(
-    commentId,
-    { text: req.params.text },
-    { new: true },
-    (err: Error) => {
+router.delete('/:id/comments/:cid', async (req: Request, res: Response) => {
+  await Plant.updateOne(
+    { _id: req.params.id },
+    { $pull: { comments: { _id: { $in: [req.params.cid] } } } },
+    {},
+    ((err: Error) => {
       if (err) {
-        return res.sendStatus(404).json({ error: 'Your comment was not modified!' });
+        return res.status(404).end();
       }
-      return res.sendStatus(200);
-    }
+      return res.status(200).end();
+    })
   );
+});
+
+router.put('/:id/comments/:cid', async (req: Request, res: Response) => {
+  await Plant.findById(req.params.id, (err, foundPlant) => {
+    if (err) {
+      return res.status(404).end();
+    }
+    foundPlant.comments = foundPlant.comments.map((comment) => {
+      if (`${comment._id}` === `${req.params.cid}`) {
+        comment.text = req.body.text;
+      }
+      return comment;
+    });
+    foundPlant.save();
+    return res.status(200).end();
+  });
 });
 
 export default router;
