734edecdd1104e74fad6ffba3d1372d2863e46c4
Ada Krupa <adriannakrupa01@gmail.com>
2021-03-04 02:07:49 +0100

    Add user controller with register and login

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as bcrypt from 'bcrypt';
+import * as mongoose from 'mongoose';
 import User from '../models/user';
 
 const router = express.Router();
@@ -10,13 +12,53 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const data = new User.User(req.body);
-  data.save((err) => {
-    if (err) console.error(err);
-    else {
-      res.sendStatus(200).end();
-    }
-  });
+  User.User.findOne({ email: req.body.email })
+    .then((user: mongoose.Document) => {
+      if (user) {
+        res.status(409).json({ error: 'Email exist!' });
+      }
+
+      return bcrypt.hash(req.body.password, 12).then((hashedPassword: string) => {
+        const newUser = new User.User({
+          login: req.body.login,
+          email: req.body.email,
+          password: hashedPassword,
+          name: req.body.name,
+          surname: req.body.surname,
+          admin: req.body.admin
+        });
+
+        newUser.save();
+        return res.sendStatus(200).end();
+      });
+    })
+    .catch((err: Error) => console.error(err));
+});
+
+router.post('/login', (req, res) => {
+  User.User.findOne({ email: req.body.email })
+    .then((user: mongoose.Document) => {
+      if (!user) {
+        res.status(404).json({ error: "User doesn't exist." });
+      } else {
+        const userPassword = user.get('password');
+        const { password } = req.body;
+
+        bcrypt
+          .compare(password, userPassword)
+          .then((doMatch: Boolean) => {
+            if (doMatch) {
+              res.status(200).end();
+            } else {
+              res.status(404).json({ error: 'Invalid password!' });
+            }
+          })
+          .catch((err: Error) => {
+            console.error(err);
+          });
+      }
+    })
+    .catch((err: Error) => console.error(err));
 });
 
 export default router;
