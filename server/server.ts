7ad543774fb062ed7d4ee32ce3a0a00f76a6907e
Suegro24 <marciu199@gmail.com>
2021-03-04 14:58:03 +0100

    config/tests

--- server/server.ts
+++ server/server.ts
@@ -4,10 +4,6 @@ import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 import './database.ts';
 
-import userController from './controllers/userController';
-import plantController from './controllers/plantController';
-import newPlantController from './controllers/newPlantController';
-
 dotenv.config();
 const app = express();
 const { PORT } = process.env;
@@ -19,9 +15,5 @@ app.use(cors());
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
-
-app.use('/api/user', userController);
-app.use('/api/plant', plantController);
-app.use('/api/newPlant', newPlantController);
 
 export default app;
