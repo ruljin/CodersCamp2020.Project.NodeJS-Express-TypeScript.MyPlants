05c2483359d45dffa50beea4a2c0ccfcd267cb21
Suegro24 <marciu199@gmail.com>
2021-03-09 23:58:47 +0100

    Delete unused code

--- server/tests/userController.spec.ts
+++ server/tests/userController.spec.ts
@@ -1,9 +1,9 @@
 import * as request from 'supertest';
-import * as app from '../server';
+import app from '../server';
 
 describe('GET /user', () => {
   it('respond with json containing a user by given id', (done) => {
-    request(app.default.server)
+    request(app)
       .get('/api/user/603bd36cd5b57809e4fd4bc3')
       .expect(200, done);
   });
