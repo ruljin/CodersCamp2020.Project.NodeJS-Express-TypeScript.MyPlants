a1252a919b7af547c6b43c741365cf1da31ee311
unknown <nilphym@gmail.com>
2021-03-12 07:59:05 +0100

    Fix test not stopping

--- server/tests/userController.spec.ts
+++ server/tests/userController.spec.ts
@@ -1,19 +1,7 @@
-import request, { agent } from 'supertest';
+import request from 'supertest';
 import app from '../app';
 
 describe('GET /user', () => {
-  let request = null;
-  let server = null;
-
-  beforeAll((done) => {
-    server = app.listen(done);
-    request = agent(server);
-  });
-
-  afterAll((done) => {
-    server.close(done);
-  });
-
   it('respond with json containing a user by given id', (done) => request(app)
     .get('/api/user/603bd36cd5b57809e4fd4bc3')
     .expect(200, done));
