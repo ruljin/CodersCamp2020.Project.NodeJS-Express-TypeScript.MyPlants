b70ac6e3f4fba954f142b19ace6b747bd5384bdc
unknown <nilphym@gmail.com>
2021-03-12 07:08:29 +0100

    store progress with fixing tests

--- server/tests/userController.spec.ts
+++ server/tests/userController.spec.ts
@@ -1,10 +1,20 @@
-import * as request from 'supertest';
-import app from '../server';
+import request, { agent } from 'supertest';
+import app from '../app';
 
 describe('GET /user', () => {
-  it('respond with json containing a user by given id', (done) => {
-    request(app)
-      .get('/api/user/603bd36cd5b57809e4fd4bc3')
-      .expect(200, done);
+  let request = null;
+  let server = null;
+
+  beforeAll((done) => {
+    server = app.listen(done);
+    request = agent(server);
   });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it('respond with json containing a user by given id', (done) => request(app)
+    .get('/api/user/603bd36cd5b57809e4fd4bc3')
+    .expect(200, done));
 });
