c28291a694288361e4cf476ec9fb62ead4a49eb1
Ada Krupa <adriannakrupa01@gmail.com>
2021-03-12 21:15:46 +0100

    Add tests for user controller

--- server/tests/userController.spec.ts
+++ server/tests/userController.spec.ts
@@ -1,8 +1,90 @@
 import request from 'supertest';
 import app from '../app';
+import { User } from '../models/user';
 
-describe('GET /user', () => {
-  it('respond with json containing a user by given id', (done) => request(app)
-    .get('/api/user/603bd36cd5b57809e4fd4bc3')
-    .expect(200, done));
+const newUser = {
+  name: 'test',
+  login: 'test',
+  surname: 'test',
+  email: 'test',
+  password: 'test',
+  admin: false,
+  notes: [],
+  plants: []
+};
+
+describe('/tests for all user', () => {
+  beforeAll(async () => {
+    await User.deleteMany({});
+  });
+
+  test('POST user (register)', (done) => {
+    request(app)
+      .post('/api/user/')
+      .send({
+        name: 'user',
+        login: 'user',
+        surname: 'user',
+        email: 'user',
+        password: 'user',
+        admin: false
+      })
+      .expect(200, done);
+  });
+
+  test('POST admin (register)', (done) => {
+    request(app)
+      .post('/api/user/')
+      .send({
+        name: 'admin',
+        login: 'admin',
+        surname: 'admin',
+        email: 'admin',
+        password: 'admin',
+        admin: true
+      })
+      .expect(200, done);
+  });
+
+  test('POST login', (done) => {
+    request(app)
+      .post('/api/user/login')
+      .send({
+        email: 'user',
+        password: 'user'
+      })
+      .expect(200, done);
+  });
+});
+
+describe('/tests for logged user (is Auth)', () => {
+  let testedUser;
+  let tokenUser;
+
+  beforeAll(async (done) => {
+    testedUser = await User.create(newUser);
+    request(app)
+      .post('/api/user/login')
+      .send({ email: 'user', password: 'user' })
+      .end((err, res) => {
+        tokenUser = res.body.token;
+        done();
+      });
+  });
+
+  test('GET user by id', (done) => {
+    request(app).get(`/api/user/${testedUser.id}`).expect(200, done);
+  });
+
+  test('PUT user', (done) => {
+    request(app)
+      .put(`/api/user/${testedUser.id}`)
+      .send({ surname: 'newSurname' })
+      .set('Authorization', `Bearer ${tokenUser}`)
+      .expect(200, done);
+  });
+
+  test('DELETE one user', (done) => {
+    request(app).delete(`/api/user/${testedUser.id}`).set('Authorization', `Bearer ${tokenUser}`).expect(200, done);
+  });
 });
