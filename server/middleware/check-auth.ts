9df625eb97bb915bdd2957d4253ae8d3dcf60a07
Ada Krupa <adriannakrupa01@gmail.com>
2021-04-10 00:29:45 +0200

    Fix authorization

--- server/middleware/check-auth.ts
+++ server/middleware/check-auth.ts
@@ -4,22 +4,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const { JWT_KEY } = process.env;
 
-export const isAuth = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, JWT_KEY);
-    req.user = decoded;
-
-    if (!req.user.admin) {
-      next();
-    } else {
-      res.status(401).json({ message: 'It is only for user.' });
-    }
-  } catch (error) {
-    res.status(401).json({ message: 'Auth failed!' });
-  }
-};
-
 export const isAdmin = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
@@ -33,5 +17,21 @@ export const isAdmin = (req, res, next) => {
     }
   } catch (error) {
     res.status(401).json({ message: 'Auth failed!' });
+  }
+};
+
+export const isAuth = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, JWT_KEY);
+    req.user = decoded;
+
+    if (req.user.admin || !req.user.admin) {
+      next();
+    } else {
+      res.status(401).json({ message: 'Not an user or admin, sorry.' });
+    }
+  } catch (error) {
+    res.status(401).json({ message: 'Authorization failed!' });
   }
 };
